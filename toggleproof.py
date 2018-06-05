14e40e44c145059313ed83febceac17cd36f384c
David Caballero <davidcaballeroe@gmail.com>
2018-06-05 16:59:53 -0500

    toggleproof.py now reads from a .xml to intialize the board

--- toggleproof.py
+++ toggleproof.py
@@ -1,5 +1,7 @@
 import tumbletiles as TT
 from sets import Set
+from getFile import getFile, parseFile
+import time
 
 #										"[bluex][bluey][redx][redy]"
 # Structure for starting and end coordinates is "21003603"
@@ -21,8 +23,14 @@ class Tree(object):
 
 
 def createTree(startingPosition):
+	# Set to hold all position that have been visited
 	visitedPositions = Set()
-	board = TT.Board(45,45)
+	
+	# Gets the empty board from the .xml file
+	boardData = parseFile("Examples/emptyboard.xml")
+	board = boardData[0]
+
+	
 
 	blueGlues = ["N","N","N","N"]
 	redGlues = ["S","S","S","S"]
@@ -46,13 +54,24 @@ def createTree(startingPosition):
 	board.Add(redPoly)
 	board.Add(bluePoly)
 
-	# for p in board.Polyominoes:
-	# 	for tile in p.Tiles:
-	# 		print "tile of id ", tile.id, " at ", tile.x, ", ", tile.y
+	
+	for c in board.ConcreteTiles:
+	 	print "concrete at ", c.x, ", ", c.y
+	for p in board.Polyominoes:
+	 	for tile in p.Tiles:
+	 		print "tile of id ", tile.id, " at ", tile.x, ", ", tile.y
+
+	for c in board.ConcreteTiles:
+	 	for x in board.ConcreteTiles:
+	 		if c != x:
+	 			if c.x == x.x and c.y == x.y:
+	 				print "tile ", c, " and tile ", x, " are both at ", c.x, ", ", c.y
+	print "number of concrete tiles: ", len(board.ConcreteTiles)
 
 if __name__ =="__main__":
         
     createTree("21003603")
+
 
 
 
